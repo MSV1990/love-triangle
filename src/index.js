2fe1b8e09437b597bbd8d87caf7066274584e850
MSV1990 <47737602+MSV1990@users.noreply.github.com>
2019-02-19 18:08:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  let count = 0;
-  for (let i = 0; i < preferences.length; i++) {
-    let first = preferences[i] - 1;
-    let second = preferences[first] - 1;
-    let third = preferences[second] - 1;
-    if (i === third) {
-      count++;
+ let arr = [];
+  if(preferences.length < 3){return count;}
+  preferences.unshift(0);
+  for (let i = 1; i < preferences.length; i++) {
+   
+    let first = i;
+    let second = preferences[preferences[i]] ;
+    let third = preferences[second];
+    if (first === third && second !== third) {
+ 
+      arr.push(first);
     }
   }
-  return Math.floor(count) / 3;
+  return arr.length / 3;
 };
-
